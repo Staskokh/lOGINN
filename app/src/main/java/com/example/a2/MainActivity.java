c7798926b475a28618bfcbf13f30056f66b12f4b
Staskokh <lion.tv.02@bk.ru>
2022-01-14 20:17:00 +0700

    fiQQWW

---
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        public void onClick (View v) {
+            Button button = (Button) v;
+            String s1 = "admin";
+            String s2 = "123456";
+    }
+
+
+    }}
